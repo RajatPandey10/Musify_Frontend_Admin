aa6a588bf64b9d345a8a3f1eca65d9c30a6beb9e
RajatPandey10 <pandeyrajat9971@gmail.com>
2026-03-06 00:11:20 +0530

    added interceptors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
 import './App.css'
+
+export const API_BASE_URL = "http://localhost:8080";
 
 function App() {
 
   return (
     <>
-      
+      Hey 
     </>
   )
 }
 
-export default App
+export default App;
